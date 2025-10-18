c87c6d5bf677fbc2588df3718dcba0c8748ecbf0
tenhou-Ravenclaw <tenhou.0126@gmail.com>
2025-10-18 12:46:04 +0900

    CORS設定を追加し、フロントエンドのオリジンを指定

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const http = require("http");
 const WebSocket = require("ws");
 const path = require("path");
@@ -6,6 +7,11 @@ const routes = require("./routes");
 const syncService = require("./services/syncService");
 
 const app = express();
+// CORS設定: 必要に応じてオリジンを指定
+app.use(cors({
+  origin: ["http://localhost:8080", "http://127.0.0.1:8080"], // フロントのURL
+  credentials: true
+}));
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
